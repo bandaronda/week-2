6801792125dc46879d1e40abe87912234adce504
bandaronda <131749924+bandaronda@users.noreply.github.com>
2023-05-16 10:17:43 +0200

    Add files via upload

---
+++ week 2/index.php
@@ -0,0 +1,47 @@
+<?php
+// opdracht 1
+$variabele1 = 10;
+$variabele2 = 10;
+
+if ($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk ";
+} else { 
+    echo "De twee waarden zijn ongelijk";
+}
+//opdracht 2
+$variabele1 = 10;
+$variabele2 = 12;
+
+if ($variabele1 != $variabele2) {
+    echo "De twee waarden zijn ongelijk";
+}
+// opdracht 3
+$variabele1 = 10;
+$variabele2 = 10;
+
+if ($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk"; 
+ } else {
+     echo "De twee waarden zijn ongelijk";
+ }
+   ?>
+
+ //opdracht 4/5
+<html>
+<head>
+</head>
+<body>
+
+<form action="index.php" method="POST">
+    <input type="text" name="naam">
+    <input type="password" name="wachtwoord">
+</form>
+    
+</body>
+</html>
+
+  
+
+
+
+
